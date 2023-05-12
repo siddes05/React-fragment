3d191a4a6572b51521a6337552226bd748c65e2f
siddes05 <siddesrajasekar@gmail.com>
2023-05-12 16:08:50 +0530

    chnages

---
+++ src/Component/FragmentOne.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FragmentOne = () => {
+  return (
+   <React.Fragment>
+    <ol>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+    </ol>
+   </React.Fragment>
+
+
+  )
+}
+
+export default FragmentOne
---
+++ src/Component/FragmentTwo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const FragmentTwo = () => {
+  return (
+    <React.Fragment>
+    <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+    </ul>
+   </React.Fragment>
+  )
+}
